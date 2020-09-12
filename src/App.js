51fac0ad7c1dfa8bf8fc6689d258c18a5919582d
tarmes <traarmes@gmail.com>
2020-09-12 00:14:43 -0400

    styled-components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Ayyyyye welcome to Trev's tutoring
         </p>
         <a
           className="App-link"
