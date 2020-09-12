2f48e249141cb144b63be4e2c5b64e38273b4e94
tarmes <traarmes@gmail.com>
2020-09-12 01:02:57 -0400

    simple nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import styled from 'styled-components';
+
+const StyledNavBar = styled.header`
+    display: flex;
+
+    nav {
+        display: flex;
+        justify-content: space-around;
+    }
+`
 
 export default function NavBar() {
     return (
-        <div className='navbar-div'>
-            <h1>THIS IS THE NAV BAR</h1>
-        </div>
+        <StyledNavBar>
+            <img
+                id='logo-img' 
+                src='https://i.ibb.co/4mJNq5p/c3bbb493-4fe4-46a8-a98a-c4c219a27348-200x200.png' 
+                alt="Can't wait to hear from you!"
+            />
+            <nav>
+                <button>About</button>
+                <button>Sign-Up</button>
+                <button>Contact</button>
+            </nav>
+        </StyledNavBar>
     )
 }
