400dfea122f1eab4623a65b0891683380ccc496c
tarmes <traarmes@gmail.com>
2020-09-19 15:27:02 -0400

    basic jumbotron on welcome

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,21 +1,23 @@
 import React from 'react'
-import logo from '../logo.svg';
+import { Jumbotron, Container } from 'reactstrap'
+import styled from 'styled-components'
+
+const Styles = styled.div`
+
+`
+
 
 export default function Welcome() {
     return (
-        <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Ayyyyye welcome to Trev's tutoring
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Styles>
+            <Jumbotron>
+                <div className='overlay'>
+                    <Container>
+                        <h1>Welcome to Trev's Tutoring</h1>
+                        <p>Tailor your learning to your needs.</p>
+                    </Container>
+                </div>
+            </Jumbotron>
+        </Styles>
     )
 }
