fe0df66c8c06b5bbc9ec7b3d7458875f71830596
tarmes <traarmes@gmail.com>
2020-09-19 12:34:01 -0400

    switched to reactstrap

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,9 @@
 import React from 'react'
-import styled from 'styled-components';
 
-const StyledNavBar = styled.header`
-    display: flex;
-
-    nav {
-        display: flex;
-        justify-content: space-around;
-    }
-`
 
 export default function NavBar() {
     return (
-        <StyledNavBar>
+        <nav>
             <img
                 id='logo-img' 
                 src='https://i.ibb.co/4mJNq5p/c3bbb493-4fe4-46a8-a98a-c4c219a27348-200x200.png' 
@@ -23,6 +14,6 @@ export default function NavBar() {
                 <button>Sign-Up</button>
                 <button>Contact</button>
             </nav>
-        </StyledNavBar>
+        </nav>
     )
 }
