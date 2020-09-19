f0c942df841a1f609e0ed2d4f4f7366d790647f4
tarmes <traarmes@gmail.com>
2020-09-19 13:13:49 -0400

    Navbar complete

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar'
 import Welcome from './components/Welcome'
+import { Route } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Welcome />
+      <Route path='/'>
+        <Navbar />
+      </Route>
+    
+      <Route exact path='/'>
+        <Welcome />
+      </Route>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,51 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import {
+  Navbar,
+  Nav,
+  NavItem,
+  NavLink,
+  NavbarBrand
+} from 'reactstrap';
 
 
 export default function NavBar() {
     return (
-        <nav>
-            <img
-                id='logo-img' 
-                src='https://i.ibb.co/4mJNq5p/c3bbb493-4fe4-46a8-a98a-c4c219a27348-200x200.png' 
-                alt="Can't wait to hear from you!"
-            />
-            <nav>
-                <button>About</button>
-                <button>Sign-Up</button>
-                <button>Contact</button>
-            </nav>
-        </nav>
+        // <nav>
+
+        //     <nav>
+        //         <button>About</button>
+        //         <button>Sign-Up</button>
+        //         <button>Contact</button>
+        //     </nav>
+        // </nav>
+
+
+        <div>
+            <Navbar color="dark" dark expand="md">
+                <NavbarBrand href="#home">
+                    <img
+                        id='logo-img' 
+                        src='https://i.ibb.co/4mJNq5p/c3bbb493-4fe4-46a8-a98a-c4c219a27348-200x200.png' 
+                        alt="Can't wait to hear from you!"
+                        width='80px'
+                        height='80px'
+                    />
+                </NavbarBrand>
+                <NavbarBrand>Tutoring by Trev</NavbarBrand>
+                <Nav className="ml-auto" navbar>
+                    <NavItem>
+                        <NavLink tag={Link} to='/'>Go home!</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink tag={Link} to='/'>About</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink tag={Link} to='/'>Contact</NavLink>
+                    </NavItem>
+                </Nav>
+            </Navbar>
+</div>
+        
     )
 }
